edb151191ba9f2a2f060212ec7d320ebef7c8be0
Jorge Kerk <jorgekerk@gmail.com>
2023-01-30 12:33:54 -0300

    Correción del README, usuario y contraseña. Ajuste del server.js

--- back/start.js
+++ back/start.js
@@ -4,5 +4,5 @@ const app = require( './src/app' )
 const PORT = 3001
 
 app.listen( PORT, ()=> {
-    console.log( 'Listening on port', PORT )
+    console.log( `Server raised in http://localhost:${ PORT }` )
 } )
